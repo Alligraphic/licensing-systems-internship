4cdb6bad3d024bc6fa2feebee0404a862e70cff9
Alireza Karimi <alireza.mathers@gmail.com>
2023-07-17 15:47:16 +0330

    add code for the sub based client

--- product2/main.cpp
+++ product2/main.cpp
@@ -1,6 +1,39 @@
 #include <iostream>
+#include "HttpClient.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    HttpClient client;
+    client.setUrl("http://127.0.0.1:8001/sub-licensing/check/");
+
+    std::string username;
+    std::string password;
+
+    std::cout << "Enter username:\n";
+    std::cin >> username;
+
+    std::cout << "Enter password:\n";
+    std::cin >> password;
+
+    std::string request = R"("{\"username\":\"")" + username + R"("\",\"password\":\")" + password + R"("\"})";
+    client.setRequest(request);
+
+    client.post();
+
+    if (client.getResponse().getData().find("true") != std::string::npos) {
+        std::cout << "Valid subscription" << std::endl;
+        system("cd ../../application && dragdroprobot.exe");
+    } else {
+        if (client.getResponse().getData().find("expired") != std::string::npos) {
+            std::cout << "Subscription expired" << std::endl;
+            std::cout << "renew in: http://127.0.0.1:8001/sub-licensing/subscriptions/" << std::endl;
+        } else if (client.getResponse().getData().find("no subscription") != std::string::npos) {
+            std::cout << "No subscription" << std::endl;
+            std::cout << "get a plan in: http://127.0.0.1:8001/sub-licensing/subscriptions/" << std::endl;
+        } else {
+            std::cout << client.getResponse().getData() << std::endl;
+            std::cout << "renew in: http://127.0.0.1:8001/auth/users/" << std::endl;
+        }
+    }
+
     return 0;
 }
